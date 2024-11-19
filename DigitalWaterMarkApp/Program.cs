92a207ad0df6fa815ae88f4123731a09367cc86f
timatet <timofei.teterin@inbox.ru>
2024-11-19 20:02:05 +0300

    fix: основные изменения для встраивания слова

--- DigitalWaterMarkApp/Program.cs
+++ DigitalWaterMarkApp/Program.cs
@@ -2,7 +2,6 @@
 // Для пакетной обработки
 //using DotSpatial.Data;
 using DigitalWaterMarkApp;
-using DigitalWaterMarkApp.AttackReproducer.Reproducers;
 using SupportLib;
 
 class Program {
@@ -26,50 +25,46 @@ class Program {
 
         MapData mapData = ShapeFileIO.Open("C:\\Users\\Heimerdinger\\Documents\\Repositories\\DigitalWaterMarkApp\\Data\\DataForDescriptor\\Init16K\\hdrLine1000i.shp");
         List<KeyValuePair<int, List<MapPoint>>> objectList = mapData.MapObjDictionary;
-        Console.WriteLine(string.Format("Objects count in map: {0}", objectList.Count));
+        Console.WriteLine(string.Format("Число объектов: {0}", objectList.Count));
 
         string test = "HEIMERDINGER"; 
 
-        Console.WriteLine("-------- ↓ ORIGINAL ↓ -----------");
+        Console.WriteLine("-------- ↓ Исходный ЦВЗ ↓ -----------");
         WaterMark waterMark = WaterMark.ConvertToWaterMark(test);
         PrintWaterMark(waterMark);
 
         MapDataProcessor mapDataProcessor = new(waterMark);
         mapDataProcessor.WaterMarkEmbeddingViaLoopingDuplicateOfPoints(mapData);
 
-        Console.WriteLine("-------- ↓ FROM LOOPING ↓ -----------");
+        Console.WriteLine("-------- ↓ Извлечение после встраивания ↓ -----------");
         var waterMarkFromLooping = MapDataProcessor.FindWMDecimalFromLoopingsInMapData(mapData);
         PrintWaterMark(waterMarkFromLooping);
         Console.WriteLine(waterMarkFromLooping.ToSecretCode());
 
-        List<IAttackReproducer> attackReproducers = new() { new DroppingAttackReproducer(), new ShufflingAttackReproducer() };
-        foreach (var attackReproducer in attackReproducers) {
-            var t = attackReproducer.GetName();
-            // TODO: reproducers running
-        }
-
-        Console.WriteLine("-------- ↓ TESTING DROP ATTACK ↓ -----------");
+        Console.WriteLine("-------- ↓ Случайное удаление объектов ↓ -----------");
         List<float> percentages = new() { 0.3F, 0.5F, 0.7F, 0.85F, 0.9F, 0.95F };
         foreach (var percentage in percentages) {
-            Console.WriteLine(string.Format("-------- ↓ FROM {0}% ↓ -----------", (int) ((1 - percentage) * 100)));
+            Console.WriteLine(string.Format("-------- ↓ Процент удаления {0}% ↓ -----------", (int) ((1 - percentage) * 100)));
             var percentOfMapData = AttackRepropducer.DropRandomPercentageOfData(percentage, mapData);
-            Console.WriteLine(string.Format("Objects count in map: {0}", percentOfMapData.ObjectsCount));
+            Console.WriteLine(string.Format("Число объектов после удаления: {0}", percentOfMapData.ObjectsCount));
             var waterMarkFromLoopingInPercentMapData = MapDataProcessor.FindWMDecimalFromLoopingsInMapData(percentOfMapData);
             PrintWaterMark(waterMarkFromLoopingInPercentMapData);
             Console.WriteLine(waterMarkFromLoopingInPercentMapData.ToSecretCode());
         }
 
-        Console.WriteLine("-------- ↓ TESTING SHUFFLE ATTACK ↓ -----------");
+        Console.WriteLine("-------- ↓ Случайное перемешивание ↓ -----------");
         var shufflingAttackResult = AttackRepropducer.ShuffleObjectsInMap(mapData);
         Console.WriteLine(string.Format("Similiar objects percentage: {0}", shufflingAttackResult.similiarObjectsPercentage));
         var waterMarkFromLoopingInShufflingMapData = MapDataProcessor.FindWMDecimalFromLoopingsInMapData(shufflingAttackResult.shufflingMapData);
         PrintWaterMark(waterMarkFromLoopingInShufflingMapData);
         Console.WriteLine(waterMarkFromLoopingInShufflingMapData.ToSecretCode());
 
-        Console.WriteLine("-------- ↓ FROM EXTRACTED ↓ -----------");
+        Console.WriteLine("-------- ↓ Извлечение после встраивания через хэш-функцию ↓ -----------");
 
         var mapDataWithWaterMark = mapDataProcessor.WaterMarkEmbedding(mapData);
         var extractedWM = MapDataProcessor.WaterMarkExtracting(mapDataWithWaterMark, waterMark.Length);
         Console.WriteLine(string.Join(' ', extractedWM.Select(i => i.ToString())));
+
+        Console.ReadKey();
     }
 }
